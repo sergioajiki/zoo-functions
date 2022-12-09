08ee53e1f7d609db92f1a7c234965b74e39f2a0f
Sergio <sergioajk@hotmail.com>
2022-12-08 22:57:36 -0300

    lint corrigido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,35 +5,13 @@ const getSpeciesByIds = (...ids) => {
   // seu código aqui
   // preciso id de species e retornar objetos
   // caminho id = data.species.id
-  // return species.filter((specieById) => specieById.id === ids) 
-  const multiplesIds = ids
-  let especieEncontrada = []
-  // console.log(multiplesIds);
+  const multiplesIds = ids;
+  const result = [];
   multiplesIds.forEach((element) => {
-    especieEncontrada = species.filter((specieById) => specieById.id === element)
-    // console.log(especieEncontrada);
-
-  })
-  return especieEncontrada
-  // result.push(especieEncontrada)
-  // console.log(result)
-
-
+    const especieEncontrada = species.find((specieById) => specieById.id === element);
+    result.push(especieEncontrada);
+  });
+  return result;
 };
-// getSpeciesByIds()
-// getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
-// getSpeciesByIds(
-//   '0938aa23-f153-4937-9f88-4858b24d6bce',
-//   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
-// );
-
-
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-// console.log(getSpeciesByIds(
-//   '0938aa23-f153-4937-9f88-4858b24d6bce',
-//   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
-// )
-// );
-
 
 module.exports = getSpeciesByIds;
