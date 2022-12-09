e5b33328f479ae461dd14573248da98527038552
Sergio <sergioajk@hotmail.com>
2022-12-09 18:30:54 -0300

    lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,6 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // retorna um objeto
-  // retorna objeto se firstname ou lastname
-  // caminho employees.firstname
-  // caminho employees.lastname
   if (employeeName === undefined) {
     return {};
   }
@@ -13,14 +9,6 @@ const getEmployeeByName = (employeeName) => {
     .find((nomeemplyee) =>
       (nomeemplyee.firstName === employeeName
       || nomeemplyee.lastName === employeeName));
-  // const firstOrLast = employees
-  //   .find((nomeemplyee) =>
-  //     (nomeemplyee.firstName === employeeName
-  //     || nomeemplyee.lastName === employeeName));
-  // return firstOrLast;
 };
-console.log(getEmployeeByName());
-console.log(getEmployeeByName('Orloff'));
-console.log(getEmployeeByName('Burl'));
 
 module.exports = getEmployeeByName;
