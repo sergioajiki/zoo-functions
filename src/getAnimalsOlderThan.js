b84bedf8bf0e9335cba46966c0dac519a88cdeee
Sergio <sergioajk@hotmail.com>
2022-12-09 16:34:54 -0300

    requisito2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  // idade do animal = species.residents.age
+  // usar every para retornar true or false
+  const selecionaAnimal = species.find((specie) => specie.name === animal);
+  return selecionaAnimal.residents
+    .every((elem) => elem.age >= age);
 };
+// console.log(getAnimalsOlderThan('penguins', 6));
 
 module.exports = getAnimalsOlderThan;
