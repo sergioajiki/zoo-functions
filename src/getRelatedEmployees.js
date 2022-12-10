d1be7907f53b25a0afbed7ce5424b728363cd14f
Sergio <sergioajk@hotmail.com>
2022-12-09 22:09:58 -0300

    requisito 4 inicio

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  // retorna booleano
+  // caminho employees.menagers
+  // verifica se é gerente usar some.
+  // console.log(id)
+   const verificaGerente = employees
+   .map((employee) => employee.managers )
+   .some((element) => element[0] === id || element[1] === id )
+    console.log(verificaGerente);
+
 };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
 };
-
+isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8')
+isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83')
+isManager('9e7d4524-363c-416a-8759-8aa7e50c0992')
+isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad')
 module.exports = { isManager, getRelatedEmployees };
