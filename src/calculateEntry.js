340f884af5f8f4573cbbcb763e535d815efdbe40
Sergio <sergioajk@hotmail.com>
2022-12-13 18:44:08 -0300

    resolvendo lint requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,5 +1,3 @@
-const en = require('faker/lib/locales/en');
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 const { prices } = require('../data/zoo_data');
 
@@ -8,69 +6,62 @@ const countEntrants = (entrants) => {
     child: 0,
     adult: 0,
     senior: 0,
-  }
+  };
   const verParametro = entrants;
   const idadeVisitantes = verParametro
     .map((visitante) => visitante.age)
     .forEach((idade) => {
       if (idade < 18) {
-        separaPorIdade.child += 1
+        separaPorIdade.child += 1;
       } else if (idade >= 50) {
-        separaPorIdade.senior += 1
+        separaPorIdade.senior += 1;
       } else {
-        separaPorIdade.adult += 1
+        separaPorIdade.adult += 1;
       }
-    })
-  return separaPorIdade
+    });
+  return separaPorIdade;
 };
 
 const calculateEntry = (entrants) => {
   const verParametro = entrants;
   if (verParametro === undefined || Object.entries(verParametro).length === 0) {
-    return 0
-  };
-  const pagantes = countEntrants(entrants)
+    return 0;
+  }
+  const pagantes = countEntrants(entrants);
   console.log(pagantes);
   console.log(prices);
-  let total = 0
+  let total = 0;
   for (let index = 0; index < Object.entries(pagantes).length; index += 1) {
-    // console.log(Object.keys(pagantes)[index])
     for (let i = 0; i < Object.keys(prices).length; i += 1) {
       if (Object.keys(pagantes)[index] === Object.keys(prices)[i]) {
-        total += (Object.values(pagantes)[index] * Object.values(prices)[i])
+        total += (Object.values(pagantes)[index] * Object.values(prices)[i]);
       }
     }
-  } return total
-  // const pagantes = Object.keys(countEntrants(entrants));
-  // const pagantes = Object.values(countEntrants(entrants));
-  // pagantes.push(countEntrants(entrants))
-  // for(index = 0; pagantes.length > 0; )
-  // const total = pagantes.forEach((pagante, index) => { 
-  // console.log(pagante[index])});
-  // console.log(total)
-
-  // seu código aqui
+  } return total;
 };
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-const visit = [
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 }
-];
-
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+// const visit = [
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 }
+// ];
 
 // console.log(countEntrants());
 // console.log(countEntrants({}));
 // console.log(countEntrants(entrants));
 // console.log(countEntrants(visit))
 // console.log(calculateEntry())
-console.log(calculateEntry(entrants))
-console.log(calculateEntry(visit))
+// console.log(calculateEntry(entrants))
+// console.log(calculateEntry(visit))
+// console.log(calculateEntry([{ name: 'Maria Costa', age: 18 }]))
+// console.log(calculateEntry([{ name: 'Lara Carvalho', age: 5 }, { name: 'Carlos Nogueira', age: 50 }
+// ]))
+
 module.exports = { calculateEntry, countEntrants };
