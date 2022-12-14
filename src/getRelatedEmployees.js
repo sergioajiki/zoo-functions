9f5717e753bf5412aa15d56e4d6b5e6bd35a07f3
Sergio <sergioajk@hotmail.com>
2022-12-14 17:32:48 -0300

    somar array?

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -25,17 +25,24 @@ const getRelatedEmployees = (managerId) => {
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!')
   }
-
+  let firstName = [];
+  let lastName = [];
   // console.log(employees)
   employees.forEach((gerente) => {
-    console.log(gerente.managers)
-    console.log(managerId)
+    // console.log(gerente.managers)
+    // console.log(managerId)
     if (gerente.managers.includes(managerId)) {
       console.log('encontrou gerente')
+      console.log(gerente.firstName)
+      console.log(gerente.lastName)
+      //pegar firstname e lastname
+      firstName.push(gerente.firstName)
+      lastName.push(gerente.lastName)
     }
   }
   )
-
+console.log(firstName)
+console.log(lastName)
 };
 // // isManager(olaId)
 // console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'))
@@ -48,9 +55,9 @@ const getRelatedEmployees = (managerId) => {
 // getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992')
 // getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad')
 
-console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+// console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 // console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 // console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
