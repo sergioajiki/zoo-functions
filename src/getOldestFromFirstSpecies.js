ce2947f1811185f70f7b2f4d26e8df75f4b2bf0a
Sergio <sergioajk@hotmail.com>
2022-12-14 23:00:15 -0300

    corrigindo lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,48 +6,22 @@ const { species } = require('../data/zoo_data');
 // console.log(species)
 
 const getOldestFromFirstSpecies = (id) => {
-  // console.log(id)
-  const primeiroAnimal = employees.find((colaborador) => colaborador.id === id).responsibleFor[0]
-  // console.log(primeiroAnimal)
-
-  // let AnimalMaisVelho = 0
-  const selecionaAnimal = speciesgit.filter((selecionaAnimal) => selecionaAnimal.id === primeiroAnimal)
-  let retornaAnimalMaisVelho = []
-  const animalMaisVelho = selecionaAnimal.filter((animal) => {
-    animal.residents
-      .sort((ageA, ageB) => ageB.age - ageA.age)
-    console.log(animal.residents[0].name)
-
-    // retornaAnimalMaisVelho.push(`${animal.residents[0].name}, ${animal.residents[0].sex}, ${animal.residents[0].age}`)
-    retornaAnimalMaisVelho.push(`${animal.residents[0].name}`)
-    retornaAnimalMaisVelho.push(`${animal.residents[0].sex}`)
-    retornaAnimalMaisVelho.push(animal.residents[0].age)
-  })
-
-
-  console.log(retornaAnimalMaisVelho)
-  return retornaAnimalMaisVelho
-  // 
-  // console.log(selecionaAnimal)
-
-
-
-  // console.log(selecionaAnimal.residents)
-  // 
-  // console.log(animalMaisVelho.residents);  
-  // })
-
-
-
-}
-
-// });
-
-//  verificaAnimal.age > idadeAnimalMaisVelho
-//  idadeAnimalMaisVelho = verificaAnimal.age
-//  console.log(idadeAnimalMaisVelho)
-// console.log(primeiroAnimalCuidado);
-
+  const primeiroAnimal = employees.find((colaborador) => colaborador.id === id).responsibleFor[0];
+  const selectAnimal = species.filter((selectAni) => selectAni.id === primeiroAnimal);
+  const retornaAnimalMaisVelho = [];
+  selectAnimal
+    .filter((animal) => {
+      animal.residents
+        .sort((ageA, ageB) => ageB.age - ageA.age);
+      // console.log(animal.residents[0].name);
+      retornaAnimalMaisVelho.push(`${animal.residents[0].name}`);
+      retornaAnimalMaisVelho.push(`${animal.residents[0].sex}`);
+      retornaAnimalMaisVelho.push(animal.residents[0].age);
+      return retornaAnimalMaisVelho;
+    });
+  console.log(retornaAnimalMaisVelho);
+  return retornaAnimalMaisVelho;
+};
 module.exports = getOldestFromFirstSpecies;
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
