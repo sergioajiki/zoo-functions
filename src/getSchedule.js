2e87a77e984596720e796e14227fbf35dbd3571e
Sergio <sergioajk@hotmail.com>
2022-12-16 20:22:34 -0300

    horario requisito 9 3/5

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,18 +2,14 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 const { hours } = require('../data/zoo_data');
 
-function diaDisponivel(scheduleTarget) {
+function diaDisponivelAnimal(scheduleTarget) {
   return species.find((specie) => specie.name === scheduleTarget).availability;
 }
 
-
 function horaFuncionamento(scheduleTarget) {
-  const horario = Object.entries(hours)
-  // console.log(hours)
-  // console.log(horario)
-  for (key in hours) {
+  for (const key in hours) {
     if (key === scheduleTarget) {
-      return `Open from ${hours[key].open}am until ${hours[key].close}pm`
+      return `Open from ${hours[key].open}am until ${hours[key].close}pm`;
       //   console.log(key)
       //   console.log(hours[key])
       //   console.log(hours[key].open)
@@ -22,78 +18,72 @@ function horaFuncionamento(scheduleTarget) {
     }
   }
 }
-// console.log(hour)
-// 
-
-
-
 
 function verificaDia(scheduleTarget) {
-  const exhibitionList = []
-  const result = {}
+  const exhibitionList = [];
+  let result = {};
   species.forEach((animal) => {
     if (animal.availability.includes(scheduleTarget)) {
-      exhibitionList.push(animal.name)
-      result[scheduleTarget] = {}
-      result[scheduleTarget].exhibition = exhibitionList
-      result[scheduleTarget].officeHour = horaFuncionamento(scheduleTarget)
+      exhibitionList.push(animal.name);
+      result[scheduleTarget] = {};
+      result[scheduleTarget].exhibition = exhibitionList;
+      result[scheduleTarget].officeHour = horaFuncionamento(scheduleTarget);
+    } else if (scheduleTarget === 'Monday') {
+      result = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
     }
-    });
-  return result
-  }
+  });
+  return result;
+}
 
 const getSchedule = (scheduleTarget) => {
   // console.log(scheduleTarget)
-  // seu código aqui
   // retorna um array com o dias da semana no formato[ 'Tuesday', 'Wednesday', 'Sunday', 'Saturday' ]
-  //
-  //caminho species.availability
-  // parametros verificados nome:animal, diaSemana, '',qualquercoisa 
+  // caminho species.availability
+  // parametros verificados nome:animal, diaSemana, '',qualquercoisa
   // if(scheduleTarget === undefined)
-
   // retorna toda a agenda
   // if(scheduleTarget === qualquerCoisa)
   // retorna toda a agenda
 
-  if (scheduleTarget === 'lions') {
-    return diaDisponivel(scheduleTarget)
-  } else {
-    // return horaFuncionamento(scheduleTarget)
-    return verificaDia(scheduleTarget)
+  if (species.some((specie) => specie.name === scheduleTarget)) {
+    console.log(species.name);
+    return diaDisponivelAnimal(scheduleTarget);
   }
-  //   const exhibition = []
-  //   const result = {}
-  //   const agendaDoDia = species.forEach((animal) => {
-  //     if (animal.availability.includes(scheduleTarget)) {
-  //       exhibition.push(animal.name)
-  //       result[scheduleTarget] = {}
-  //       result[scheduleTarget].exhibition = exhibition
-  //       result[scheduleTarget].officeHour = `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`
-  //       result[scheduleTarget].officeHour = `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`
-  //     }
-  //   });
-  // console.log(result)
-  // return result
-  // return dias
+  // return horaFuncionamento(scheduleTarget)
+  return verificaDia(scheduleTarget);
+};
+//   const exhibition = []
+//   const result = {}
+//   const agendaDoDia = species.forEach((animal) => {
+//     if (animal.availability.includes(scheduleTarget)) {
+//       exhibition.push(animal.name)
+//       result[scheduleTarget] = {}
+//       result[scheduleTarget].exhibition = exhibition
+//       result[scheduleTarget].officeHour = `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`
+//       result[scheduleTarget].officeHour = `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`
+//     }
+//   });
+// console.log(result)
+// return result
+// return dias
 
-  // return result
+// return result
 
-  // console.log(exhibition);
-  // console.log(result)
-}
-
-
-
-
+// console.log(exhibition);
+// console.log(result)
 // getSchedule('Tuesday')
 // console.log(getSchedule('lions'))
+console.log(getSchedule('Monday'));
 console.log(getSchedule('Tuesday'));
+console.log(getSchedule('Thursday'));
+console.log(getSchedule('lions'));
+console.log(getSchedule('penguins'));
+
 // {
 //   'Tuesday': {
 //     'officeHour': 'Open from 8am until 6pm',
 //       'exhibition': ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
 // },
 // }
-
 
 module.exports = getSchedule;
