ba8900a72526d4491b5c121bdb758c7c158a50ff
Sergio <sergioajk@hotmail.com>
2022-12-20 14:38:06 -0300

    requito 13 infofuncionaro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -12,39 +12,86 @@ const { employees } = require('../data/zoo_data');
 // undefined retorna todos os trabalhadores
 // se não encontrada retorna throw new Error('Informações inválidas');
 
-function procuraFuncionario(parametro) {
-  const result = {}
-  // console.log(funcionario.name)
+function idFuncionario(funcionario) {
+  let result = ''
   employees.find((employee) => {
-    if (employee.firstName === parametro.name
-      || employee.lastName === parametro.name
-      || employee.id === parametro.id) {
-        result.id = employee.id
-        result.fullName = `${employee.firstName} ${employee.lastName}`;
-        result.species = employee.responsibleFor
-      // console.log(employee.firstName, employee.lastName)
-      // console.log(employee.responsibleFor)
-      // console.log(result);
-    } else {
-      throw new Error('Informações inválidas');
+    if (employee.firstName === funcionario.name
+      || employee.lastName === funcionario.name
+      || employee.id === funcionario.id) {
+      result = employee.id
     }
-
-
+    //  else {
+    //   console.log('teste')
+    //   throw new Error('Informações inválidas');
+    // }
   })
-  return result 
-}
-
-const getEmployeesCoverage = (parametro) => {
-    if (parametro === undefined){
-      return 'colocar agenda completa'
+  return result;
+};
+// function nomeCompleto(parametro) {
+//   let fullName = '';
+//   employees.find((employee) => {
+//     if (employee.id === idFuncionario(parametro)) {
+//       fullName = `${employee.firstName} ${employee.lastName}`;
+//     }
+//   })
+//   return fullName;
+// };
+function infoFuncionario(parametro) {
+  let info = {}
+  let fullName = '';
+  employees.find((employee) => {
+    if (employee.id === idFuncionario(parametro)) {
+      fullName = `${employee.firstName} ${employee.lastName}`;
     }
-   return procuraFuncionario(parametro)
+  })
+  info.id = idFuncionario(parametro)
+  info.fullName = fullName
+  Object.assign(info, getAnimals(parametro))
 
+  return info;
+};
 
+function responsavelPorAnimais(parametro) {
+  let animais = [];
+  const animaisCuidados = employees.find((employee) => {
+    if (employee.id === idFuncionario(parametro)) {
+      animais = employee.responsibleFor
+      // return animais;
+    }
+  })
+  return animais
+}
+function getAnimals(parametro) {
+  let animal = [];
+  let local = [];
+  let animalAndLocal = {};
+  console.log(responsavelPorAnimais(parametro))
+  species.forEach((specie) => {
+    if (responsavelPorAnimais(parametro).includes(specie.id)) {
+      // console.log(specie.id)
+      animal.push(specie.name)
+      local.push(specie.location)
+    }
+  })
+  animalAndLocal.species = animal
+  animalAndLocal.location = local
+  return animalAndLocal
+};
+const getEmployeesCoverage = (parametro) => {
+
+  if (parametro === undefined) {
+    return 'colocar agenda completa'
+  }
+  // validaFuncionario(parametro)
+  return infoFuncionario(parametro)
+  // return getAnimals(parametro)
+  // return responsavelPorAnimais(parametro)
+  // return nomeCompleto(parametro)
+  // return idFuncionario(parametro)
 };
 console.log(getEmployeesCoverage({ name: 'Sharonda' }));
-// console.log(getEmployeesCoverage({ name: 'Spry' }));
-// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
-console.log(getEmployeesCoverage());
-console.log(getEmployeesCoverage({ name: 'sergio' }));
+console.log(getEmployeesCoverage({ name: 'Spry' }));
+console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage());
+// console.log(getEmployeesCoverage({ name: 'Sergio' }));
 module.exports = getEmployeesCoverage;
