1a73432fd7f6bb14efefa990562c037e9e2dbe11
Sergio <sergioajk@hotmail.com>
2022-12-21 15:06:01 -0300

    requisito 11 verificar lancamento de erro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,3 +1,4 @@
+// const { getVersion } = require('jest');
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
@@ -8,23 +9,35 @@ const { employees } = require('../data/zoo_data');
 //   species: [ "otters", "frogs" ], // espécies as quais a pessoa é responsável
 //   locations: [ "SE", "SW" ], // Um array contendo todas as localizações das espécies
 // }
-// parametro nome, sobrenome , id, undefined, 
+// parametro nome, sobrenome , id, undefined,
 // undefined retorna todos os trabalhadores
 // se não encontrada retorna throw new Error('Informações inválidas');
 
+function verificaFuncionario(parametro) {
+  return employees
+    .some((employee) => employee.firstName === parametro.name
+      || employee.lastName === parametro.name
+      || employee.id === parametro.id);
+}
+
 function idFuncionario(parametro) {
   // console.log(parametro)
-  let result = ''
+  let result = '';
+  // console.log(employees);
   employees.find((employee) => {
+    // console.log(employee);
     if (employee.firstName === parametro.name
       || employee.lastName === parametro.name
       || employee.id === parametro.id) {
-      result = employee.id
+      result = employee.id;
     }
-  })
-  // console.log(result)
+    return result;
+  });
+  // if (result === '') {
+  //   throw new Error('Informações inválidas');
+  // }
   return result;
-};
+}
 
 function nomeCompleto(id) {
   // console.log(id)
@@ -33,27 +46,30 @@ function nomeCompleto(id) {
     if (employee.id === idFuncionario(id)) {
       fullName = `${employee.firstName} ${employee.lastName}`;
     }
-  })
+    return fullName;
+  });
   // console.log(fullName)
-  return fullName
-};
+  return fullName;
+}
 
 function responsavelPorAnimais(id) {
   // console.log(id)
   let animais = [];
-  const animaisCuidados = employees.find((employee) => {
+  employees.map((employee) => {
     if (employee.id === idFuncionario(id)) {
       animais = employee.responsibleFor;
       // return animais;
     }
-  })
-  return animais
-};
+    // console.log('teste',animais);
+    return animais;
+  });
+  return animais;
+}
 
 function getAnimals(id) {
-  let animal = [];
-  let local = [];
-  let animalAndLocal = {};
+  const animal = [];
+  const local = [];
+  const animalAndLocal = {};
   // console.log(responsavelPorAnimais(parametro))
   species.forEach((specie) => {
     if (responsavelPorAnimais(id).includes(specie.id)) {
@@ -61,54 +77,57 @@ function getAnimals(id) {
       animal.push(specie.name);
       local.push(specie.location);
     }
-  })
+  });
   animalAndLocal.species = animal;
   animalAndLocal.locations = local;
   return animalAndLocal;
-};
+}
 
 function infoFuncionario(id) {
   // console.log(id)
-  let info = {};
+  const info = { id };
   // console.log(nomeCompleto(id))
-  info.id = idFuncionario(id)
-  info.fullName = nomeCompleto(id)
-  Object.assign(info, getAnimals(id))
+  // console.log('teste', idFuncionario(id))
+  info.id = idFuncionario(id);
+  // console.log('teste', nomeCompleto(id))
+  info.fullName = nomeCompleto(id);
+  Object.assign(info, getAnimals(id));
   // console.log(info)
   return info;
 }
 
-const allFuncId = employees.map(employee => ( { id: employee.id }));
-  // console.log(allFuncId)
+const allFuncId = employees.map((employee) => ({ id: employee.id }));
+// console.log(allFuncId)
 
 function allFuncionarios() {
-  let fullFunc = [];
+  const fullFunc = [];
   allFuncId.forEach((idFunc) => {
     // console.log(idFunc)
     // fullFunc = infoFuncionario(idFunc)
     fullFunc.push(infoFuncionario(idFunc));
-     
-    })
-//  console.log(fullFunc)
-return fullFunc
-};
+  });
+  //  console.log(fullFunc)
+  return fullFunc;
+}
 
 const getEmployeesCoverage = (parametro) => {
-
   if (parametro === undefined) {
-     return allFuncionarios()
+    return allFuncionarios();
   }
-  // validaFuncionario(parametro)
-  return infoFuncionario(parametro)
+  if (verificaFuncionario(parametro) === false) {
+    throw new Error('Informações Inválidas');
+  }
+  return infoFuncionario(parametro);
+  // console.log(verificaFuncionario(parametro))
   // return getAnimals(parametro)
   // return responsavelPorAnimais(parametro)
   // return nomeCompleto(parametro)
   // return idFuncionario(parametro)
 };
-// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 // console.log(getEmployeesCoverage({ name: 'Spry' }));
-// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage({ id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1' }));
+
 // console.log(getEmployeesCoverage());
 // console.log(getEmployeesCoverage({ name: 'Sergio' }));
 module.exports = getEmployeesCoverage;
-
