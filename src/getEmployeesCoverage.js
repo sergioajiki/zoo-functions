24a71b638391ccb2d5840e392494f23095f2a1cb
Sergio <sergioajk@hotmail.com>
2022-12-20 22:19:46 -0300

    requito 13 4/5

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -12,75 +12,91 @@ const { employees } = require('../data/zoo_data');
 // undefined retorna todos os trabalhadores
 // se não encontrada retorna throw new Error('Informações inválidas');
 
-function idFuncionario(funcionario) {
+function idFuncionario(parametro) {
+  // console.log(parametro)
   let result = ''
   employees.find((employee) => {
-    if (employee.firstName === funcionario.name
-      || employee.lastName === funcionario.name
-      || employee.id === funcionario.id) {
+    if (employee.firstName === parametro.name
+      || employee.lastName === parametro.name
+      || employee.id === parametro.id) {
       result = employee.id
     }
-    //  else {
-    //   console.log('teste')
-    //   throw new Error('Informações inválidas');
-    // }
   })
+  // console.log(result)
   return result;
 };
-// function nomeCompleto(parametro) {
-//   let fullName = '';
-//   employees.find((employee) => {
-//     if (employee.id === idFuncionario(parametro)) {
-//       fullName = `${employee.firstName} ${employee.lastName}`;
-//     }
-//   })
-//   return fullName;
-// };
-function infoFuncionario(parametro) {
-  let info = {}
+
+function nomeCompleto(id) {
+  // console.log(id)
   let fullName = '';
   employees.find((employee) => {
-    if (employee.id === idFuncionario(parametro)) {
+    if (employee.id === idFuncionario(id)) {
       fullName = `${employee.firstName} ${employee.lastName}`;
     }
   })
-  info.id = idFuncionario(parametro)
-  info.fullName = fullName
-  Object.assign(info, getAnimals(parametro))
-
-  return info;
+  // console.log(fullName)
+  return fullName
 };
 
-function responsavelPorAnimais(parametro) {
+function responsavelPorAnimais(id) {
+  // console.log(id)
   let animais = [];
   const animaisCuidados = employees.find((employee) => {
-    if (employee.id === idFuncionario(parametro)) {
-      animais = employee.responsibleFor
+    if (employee.id === idFuncionario(id)) {
+      animais = employee.responsibleFor;
       // return animais;
     }
   })
   return animais
-}
-function getAnimals(parametro) {
+};
+
+function getAnimals(id) {
   let animal = [];
   let local = [];
   let animalAndLocal = {};
-  console.log(responsavelPorAnimais(parametro))
+  // console.log(responsavelPorAnimais(parametro))
   species.forEach((specie) => {
-    if (responsavelPorAnimais(parametro).includes(specie.id)) {
+    if (responsavelPorAnimais(id).includes(specie.id)) {
       // console.log(specie.id)
-      animal.push(specie.name)
-      local.push(specie.location)
+      animal.push(specie.name);
+      local.push(specie.location);
     }
   })
-  animalAndLocal.species = animal
-  animalAndLocal.location = local
-  return animalAndLocal
+  animalAndLocal.species = animal;
+  animalAndLocal.locations = local;
+  return animalAndLocal;
 };
+
+function infoFuncionario(id) {
+  // console.log(id)
+  let info = {};
+  // console.log(nomeCompleto(id))
+  info.id = idFuncionario(id)
+  info.fullName = nomeCompleto(id)
+  Object.assign(info, getAnimals(id))
+  // console.log(info)
+  return info;
+}
+
+const allFuncId = employees.map(employee => ( { id: employee.id }));
+  // console.log(allFuncId)
+
+function allFuncionarios() {
+  let fullFunc = [];
+  allFuncId.forEach((idFunc) => {
+    // console.log(idFunc)
+    // fullFunc = infoFuncionario(idFunc)
+    fullFunc.push(infoFuncionario(idFunc));
+     
+    })
+//  console.log(fullFunc)
+return fullFunc
+};
+
 const getEmployeesCoverage = (parametro) => {
 
   if (parametro === undefined) {
-    return 'colocar agenda completa'
+     return allFuncionarios()
   }
   // validaFuncionario(parametro)
   return infoFuncionario(parametro)
@@ -89,9 +105,10 @@ const getEmployeesCoverage = (parametro) => {
   // return nomeCompleto(parametro)
   // return idFuncionario(parametro)
 };
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
-console.log(getEmployeesCoverage({ name: 'Spry' }));
-console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Spry' }));
+// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
 // console.log(getEmployeesCoverage());
 // console.log(getEmployeesCoverage({ name: 'Sergio' }));
 module.exports = getEmployeesCoverage;
+
